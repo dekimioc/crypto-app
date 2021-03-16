e9de17abcdef45bb3c4da884fd729693049364ec
dekimioc <dekimioc@yahoo.com>
2021-03-16 01:34:57 +0100

    listed some values from API into componentes and added propTypes validation

--- src/components/MainTable/MainTable.js
+++ src/components/MainTable/MainTable.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import TableRow from '../TableRow/TableRow'
 
 const MainTable = () => {
+  const [data, setData] = useState([])
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_CRYPTO}`, { headers: { 'X-CMC_PRO_API_KEY': `${process.env.REACT_APP_CRYPTO_API_KEY}` } })
       .then(response => {
         // handle success
-        console.log(response)
+        if (response.status === 200) {
+          setData(response.data.data)
+        }
       })
       .catch(error => {
         // handle error
         console.log(error)
       })
-  })
+  }, [])
   return (
     <table>
     <thead>
@@ -26,7 +29,16 @@ const MainTable = () => {
             <th>$ Value of Your Coin</th>
         </tr>
     </thead>
-        <TableRow />
+<tbody>
+    {data.map(e => <TableRow
+                        key={e.id}
+                        name={e.name}
+                        symbol={e.symbol}
+                        price={e.quote.USD.price}
+                        percent={e.quote.USD.percent_change_24h}
+                    />
+    )}
+</tbody>
     </table>
   )
 }
--- src/components/TableRow/TableRow.js
+++ src/components/TableRow/TableRow.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const TableRow = () => {
+const TableRow = ({ name, symbol, price, percent }) => {
   return (
     <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>{name}</td>
+        <td>{symbol}</td>
+        <td>$ {price.toFixed(2)}</td>
+        <td>{percent.toFixed(2)} %</td>
+        <td>input</td>
+        <td>$ </td>
     </tr>
   )
 }
 
+TableRow.propTypes = {
+  name: PropTypes.string.isRequired,
+  symbol: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  percent: PropTypes.number.isRequired
+}
 export default TableRow
