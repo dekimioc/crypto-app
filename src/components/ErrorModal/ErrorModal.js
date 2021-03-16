3bc27f3d7599a25b730f4026e25b57503af62fb3
dekimioc <dekimioc@yahoo.com>
2021-03-16 23:39:51 +0100

    finished app

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 
 const ErrorModal = ({ errorText }) => {
   return (
-    <div className="error-message-container text-center mt-5">
+    <div className="error-message-container">
         <h1 className="color-error">{errorText}</h1>
     </div>
   )
